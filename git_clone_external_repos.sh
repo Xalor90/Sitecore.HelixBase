efd6b9912369b95d41109a11910e7282f3644df0
Derek Temple <xalor90@outlook.com>
2018-07-15 09:35:45 -0700

    Set up Unicorn

--- git_clone_external_repos.sh
+++ git_clone_external_repos.sh
@@ -4,6 +4,14 @@
 root="C:\\\Users\\\xalor\\\source\\\repos\\\Sitecore.HelixBase"
 web="src/Project/HelixBase/Sitecore.HelixBase.Website"
 
+# Create Unicorn files for web project
+location="$web/App_Config/Include/Unicorn/"
+file="Unicorn.Configs.Sitecore.System"
+
+cp "$location/$file.example" "$location/$file.config"
+
+sed -i "s/\[Replace with Solution path\]/$root/g" "$location/$file.config"
+
 # Import Sitecore.Foundation.Extension.DynamicPlaceholderRendering Repo
 repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.DynamicPlaceholderRendering.git"
 location="src/Foundation/Extension/Sitecore.Foundation.Extension.DynamicPlaceholderRendering"
