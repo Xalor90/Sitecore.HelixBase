b9a48bc86577ff8ee11fcb21f377e3694de1297b
Derek Temple <xalor90@outlook.com>
2018-07-17 14:20:36 -0400

    Added ability to choose which modules get installed.

--- git_clone_external_repos.sh
+++ git_clone_external_repos.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Import all external git repositories to proper locations
 
+# Set these variables before running this script:
 root="C:\\\Users\\\xalor\\\source\\\repos\\\Sitecore.HelixBase"
 web="src/Project/HelixBase/Sitecore.HelixBase.Website"
+
+installDynamicPlaceholderRendering=true
+installFieldType=true
+installMediaType=true
+installResponsiveImage=true
+installStandardValueCustomToken=true
 
 # Create Unicorn files for web project
 location="$web/App_Config/Include/Unicorn/"
@@ -13,77 +20,87 @@ cp "$location/$file.example" "$location/$file.config"
 sed -i "s/\[Replace with Solution path\]/$root/g" "$location/$file.config"
 
 # Import Sitecore.Foundation.Extension.DynamicPlaceholderRendering Repo
-repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.DynamicPlaceholderRendering.git"
-location="src/Foundation/Extension/Sitecore.Foundation.Extension.DynamicPlaceholderRendering"
+if [ installDynamicPlaceholderRendering ]; then
+	repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.DynamicPlaceholderRendering.git"
+	location="src/Foundation/Extension/Sitecore.Foundation.Extension.DynamicPlaceholderRendering"
 
-rm -rf "$location"
-git clone "$repo" "$location"
+	rm -rf "$location"
+	git clone "$repo" "$location"
 
-dirPath="App_Config/Include/Foundation/Extension/"
-dir="DynamicPlaceholderRendering/"
+	dirPath="App_Config/Include/Foundation/Extension/"
+	dir="DynamicPlaceholderRendering/"
 
-mkdir -p "$web/$dirPath"
-rm -rf "$web/$dirPath/$dir"
-cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+	mkdir -p "$web/$dirPath"
+	rm -rf "$web/$dirPath/$dir"
+	cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+fi
 
 # Import Sitecore.Foundation.Extension.FieldType Repo
-repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.FieldType.git"
-location="src/Foundation/Extension/Sitecore.Foundation.Extension.FieldType"
+if [ installFieldType ]; then
+	repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.FieldType.git"
+	location="src/Foundation/Extension/Sitecore.Foundation.Extension.FieldType"
 
-rm -rf "$location"
-git clone "$repo" "$location"
+	rm -rf "$location"
+	git clone "$repo" "$location"
 
-file="App_Config/Include/Unicorn/Unicorn.Configs.Foundation.Extension.FieldType.config"
+	file="App_Config/Include/Unicorn/Unicorn.Configs.Foundation.Extension.FieldType.config"
 
-cp	"$location/$file" "$web/$file"
+	cp	"$location/$file" "$web/$file"
 
-sed -i "s/\[Replace with Solution path\]/$root/g" "$web/$file"
+	sed -i "s/\[Replace with Solution path\]/$root/g" "$web/$file"
+fi
 
 # Import Sitecore.Foundation.Extension.MediaType Repo
-repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.MediaType.git"
-location="src/Foundation/Extension/Sitecore.Foundation.Extension.MediaType"
+if [ installMediaType ]; then
+	repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.MediaType.git"
+	location="src/Foundation/Extension/Sitecore.Foundation.Extension.MediaType"
 
-rm -rf "$location"
-git clone "$repo" "$location"
+	rm -rf "$location"
+	git clone "$repo" "$location"
 
-dirPath="App_Config/Include/Foundation/Extension/"
-dir="MediaType/"
+	dirPath="App_Config/Include/Foundation/Extension/"
+	dir="MediaType/"
 
-mkdir -p "$web/$dirPath"
-rm -rf "$web/$dirPath/$dir"
-cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+	mkdir -p "$web/$dirPath"
+	rm -rf "$web/$dirPath/$dir"
+	cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
 
-dirPath="sitecore/shell/"
-dir="override/"
+	dirPath="sitecore/shell/"
+	dir="override/"
 
-mkdir -p "$web/$dirPath"
-rm -rf "$web/$dirPath/$dir"
-cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+	mkdir -p "$web/$dirPath"
+	rm -rf "$web/$dirPath/$dir"
+	cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+fi
 
 # Import Sitecore.Foundation.Extension.ResponsiveImage Repo
-repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.ResponsiveImage.git"
-location="src/Foundation/Extension/Sitecore.Foundation.Extension.ResponsiveImage"
+if [ installResponsiveImage ]; then
+	repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.ResponsiveImage.git"
+	location="src/Foundation/Extension/Sitecore.Foundation.Extension.ResponsiveImage"
 
-rm -rf "$location"
-git clone "$repo" "$location"
+	rm -rf "$location"
+	git clone "$repo" "$location"
 
-dirPath="App_Config/Include/Foundation/Extension/"
-dir="ResponsiveImage/"
+	dirPath="App_Config/Include/Foundation/Extension/"
+	dir="ResponsiveImage/"
 
-mkdir -p "$web/$dirPath"
-rm -rf "$web/$dirPath/$dir"
-cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+	mkdir -p "$web/$dirPath"
+	rm -rf "$web/$dirPath/$dir"
+	cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+fi
 
 # Import Sitecore.Foundation.Extension.StandardValueCustomToken Repo
-repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.StandardValueCustomToken.git"
-location="src/Foundation/Extension/Sitecore.Foundation.Extension.StandardValueCustomToken"
+if [ installStandardValueCustomToken ]; then
+	repo="https://github.com/Xalor90/Sitecore.Foundation.Extension.StandardValueCustomToken.git"
+	location="src/Foundation/Extension/Sitecore.Foundation.Extension.StandardValueCustomToken"
 
-rm -rf "$location"
-git clone "$repo" "$location"
+	rm -rf "$location"
+	git clone "$repo" "$location"
 
-dirPath="App_Config/Include/Foundation/Extension/"
-dir="StandardValueCustomToken/"
+	dirPath="App_Config/Include/Foundation/Extension/"
+	dir="StandardValueCustomToken/"
 
-mkdir -p "$web/$dirPath"
-rm -rf "$web/$dirPath/$dir"
-cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+	mkdir -p "$web/$dirPath"
+	rm -rf "$web/$dirPath/$dir"
+	cp -R "$location/$dirPath/$dir" "$web/$dirPath/$dir"
+fi
